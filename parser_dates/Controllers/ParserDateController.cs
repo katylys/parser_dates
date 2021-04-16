79b104789dbe84f28399ab963bbffc0f6258c3d5
Екатерина Лысыченко <katylys@MacBook-Pro-Ekaterina.local>
2021-04-16 12:11:34 +0300

    add project to git

---
+++ parser_dates/Controllers/ParserDateController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hors;
+
+namespace parser_dates.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParserDateController : ControllerBase
+    {
+        private readonly ILogger<ParserDateController> _logger;
+
+        public ParserDateController(ILogger<ParserDateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ParserDate Post(Request request)
+        {
+            var parser = new HorsTextParser();
+            var result = parser.Parse(request.Text, DateTime.Now);
+            var text = result.Text;
+            //var formatText = result.TextWithTokens;
+            ParserDate ret;
+            if (result.Dates != null && result.Dates.Count > 0)
+            {
+                var date = result.Dates[0].DateFrom;
+                ret = new ParserDate
+                {
+                    ParsingDate = date.ToString(),
+                    OtherText = text
+                };
+            }
+            else
+            {
+                ret = new ParserDate
+                {
+                    ParsingDate = "",
+                    OtherText = request.Text
+                };
+            }
+            
+            return ret;
+        }
+    }
+}
---
+++ parser_dates/ParserDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace parser_dates
+{
+    public class Request
+    {
+        public string Text { get; set; }
+    }
+
+    public class ParserDate
+    {
+        public string ParsingDate { get; set; }
+        public string OtherText { get; set; }
+    }
+
+}
